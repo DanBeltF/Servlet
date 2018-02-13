ec43f870a7434c3bbb5c2573dcb6b2f82bdc8f14
Eduardo Arias <julianeduardoarias@outlook.com>
2018-02-12 21:08:26 -0500

    Terminado

--- src/main/java/edu/eci/pdsw/NewHttpServlet.java
+++ src/main/java/edu/eci/pdsw/NewHttpServlet.java
@@ -31,9 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NewHttpServlet extends HttpServlet{
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-       
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {      
         DataSourceStub ds = DataSourceStub.getInstance();
         try {
             Optional<Integer> optName = Optional.ofNullable(Integer.valueOf(req.getParameter("id")));
@@ -75,7 +73,48 @@ public class NewHttpServlet extends HttpServlet{
             responseWriter.flush();
         }
     }
-    
-    
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DataSourceStub ds = DataSourceStub.getInstance();
+        try {
+            Optional<Integer> optName = Optional.ofNullable(Integer.valueOf(req.getParameter("id")));
+            Client c = ds.getClientById(optName.get());
+            String[] info = new String[]{c.getName(),c.getEmail(),c.getAddress()};
+            String htmlinfo = 
+                    "<!DOCTYPE html>" +
+                    "<body>" +
+                    "<table class=\"client\" cellspacing=\"11\" cellpadding=\"10\" border=\"1\">" +
+                    "<tr>" +
+                    "    <th>ID</th>" +
+                    "    <th>Nombre</th>" +
+                    "    <th>Email</th>" +
+                    "    <th>Direccion</th>" +
+                    "  </tr>" +
+                    "  <tr>" +
+                    "    <th>"+optName.get()+"</th>" +
+                    "    <td>"+info[0]+"</td>" +
+                    "    <td>"+info[1]+"</td>" +
+                    "    <td>"+info[2]+"</td>"  +
+                    "</table>" +
+                    "</body>" +
+                    "</html>";
+            resp.setStatus(200);
+            Writer responseWriter=resp.getWriter();
+            responseWriter.write(htmlinfo);
+            responseWriter.flush();            
+        } catch (ClientNotFoundException ex) {
+            resp.setStatus(400);
+            Writer responseWriter=resp.getWriter();
+            responseWriter.write(404 +" No existe el cliente con el identificador dado");       
+            Logger.getLogger(NewHttpServlet.class.getName()).log(Level.SEVERE, null, ex);
+            responseWriter.flush();
+        }catch(NumberFormatException ex){
+            resp.setStatus(400);
+            Writer responseWriter=resp.getWriter();
+            responseWriter.write(400 +" Identificador dado no valido");  
+            responseWriter.flush();
+        }
+    }  
     
 }
